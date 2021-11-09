ad35c9bece5e89d409a9a3989ada93a91815bcde
Marc <7955883+wraix@users.noreply.github.com>
2021-11-09 12:53:14 +0100

    Test reviewdog gofmt

--- middleware/logging.go
+++ middleware/logging.go
@@ -9,6 +9,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+
+
 func WithLogging() (MiddlewareHandler) {
   return func(next http.Handler) http.Handler {
   	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
